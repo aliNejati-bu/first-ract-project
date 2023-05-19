6c786f90bedb83f0f4dd4fecac3cb933b7a35f86
aliNejati-bu <electrocellco@gmail.com>
2023-05-19 15:59:51 +0330

    Set React Elements in TodoList component.

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,14 +1,25 @@
 import TodoItem from "../TodoItem/TodoItem";
+import react from 'react';
 
 function TodoList() {
-    return (
+    return react.createElement(
+        'ul',
+        {
+            className:'home__list'
+        },
+        react.createElement(TodoItem,{},'Reza'),
+        react.createElement(TodoItem,{},'Mohammad'),
+        react.createElement(TodoItem,{},'Sadegh'),
+        react.createElement(TodoItem,{},'Ali'),
+    );
+    /*return (
         <ul className="home__list">
             <TodoItem>Reza</TodoItem>
             <TodoItem>Mohammad</TodoItem>
-            <TodoItem>sadegh</TodoItem>
-            <TodoItem>mohammad</TodoItem>
+            <TodoItem>Sadegh</TodoItem>
+            <TodoItem>Ali</TodoItem>
         </ul>
-    );
+    );*/
 }
 
 export default TodoList;
