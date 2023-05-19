5b870e93022ccba8a411e08a18926964df53a9e2
aliNejati-bu <electrocellco@gmail.com>
2023-05-19 19:28:54 +0330

    add state to delete BTN.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from "./components/Header/Header";
-import Container from "./components/Container/Container";
 import Main from "./components/Main/Main";
 
 function App() {
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,19 @@
+import React, {useState} from "react";
+
 import './TodoItem.css';
 import Button from "../button/Button";
 
 function TodoItem(props) {
+    let t = props.children;
+    const [a, setA] = useState(t);
+
+    const deleteHandler = () => {
+        setA('------------------------------------------');
+    }
     return (
         <li className="home__list_item">
-            <span>{props.children}</span>
-            <Button btnType='delete'>X</Button>
+            <span>{a}</span>
+            <Button btnType='delete' onClick={deleteHandler}>X</Button>
         </li>
     );
 }
