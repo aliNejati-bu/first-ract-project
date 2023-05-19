b86f0f4a925828b52f6a2effbb30319cf3899a43
aliNejati-bu <electrocellco@gmail.com>
2023-05-19 12:49:11 +0330

    fix style sizes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
     return (
         <ul className="home__list">
             <TodoItem name="reza"></TodoItem>
+            <TodoItem name="gyut"></TodoItem>
+            <TodoItem name="rgyuugeza"></TodoItem>
+            <TodoItem name="rgyugeza"></TodoItem>
         </ul>
     );
 }
