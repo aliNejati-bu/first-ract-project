c3f8ac64f7ee09dc338bffb6d4f85ecde304d7c2
aliNejati-bu <electrocellco@gmail.com>
2023-06-17 03:32:43 +0330

    add key to TODO list.

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,17 +4,15 @@ const TodoList = (props) => {
     const deleteHandler = (id) => {
         props.onDeleteItem(id);
     };
-    let list_items = <p>TODO List is empty.</p>
     if (props.items.length > 0) {
-        list_items = <ul className="home__list">
+        return <ul className="home__list">
             {props.items.map((todo) => <TodoItem key={todo.id} onDeleteItem={deleteHandler}>{todo}</TodoItem>)}
         </ul>;
+    } else {
+        return <p>TODO List is empty.</p>;
     }
 
 
-    return (<>
-        {list_items}
-    </>);
 }
 
 export default TodoList;
